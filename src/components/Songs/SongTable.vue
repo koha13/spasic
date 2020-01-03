9718a7793b74287d6f9fcdecb831e87351020150
khoa <policekhoa13101998@gmail.com>
2020-01-04 00:18:23 +0700

    Add view for table songs

--- src/components/Songs/SongTable.vue
+++ src/components/Songs/SongTable.vue
@@ -7,11 +7,10 @@
           <th id="r2" scope="col">Name</th>
           <th id="r3" scope="col">Artists</th>
           <th id="r4" scope="col">Time</th>
-          <th id="mobile-only" scope="col"></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(song,index) in allSongs" :key="index">
+        <tr v-for="(song,index) in allSongs" :key="index" :class="{current: index==2}">
           <th id="r1">{{index+1}}</th>
           <td id="r2" style="word-wrap: break-word;">{{song.name}}</td>
           <td id="r3">{{song.artists}}</td>
@@ -32,3 +31,9 @@ export default {
   }
 };
 </script>
+<style scoped>
+.current {
+  color: orange;
+  background-color: rgba(0, 0, 0, 0.075);
+}
+</style>
