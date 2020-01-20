3d86a99de8dceebb563a0c5c6e8b0d24451703de
khoa <policekhoa13101998@gmail.com>
2020-01-20 22:52:38 +0700

    Change css card listsong

--- src/components/Songs/SongCards.vue
+++ src/components/Songs/SongCards.vue
@@ -13,7 +13,7 @@
       >
         <div id="image-1" style="width: 100%; height: 150px;" @click="playSong(song)">
           <img
-            style="width:100%; height:150px; object-fit: cover; border-radius:10px"
+            style="width:100%; height:150px; object-fit: cover; border-radius:3px"
             v-lazy="song.avatar"
           />
           <div id="middle">
