a03f5bd0b98830ef4e319ce261eea02e8541d62e
khoa <policekhoa13101998@gmail.com>
2020-04-24 22:47:25 +0700

    Change view for profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="profile container">
-    <div class="row">
-      <h1 class="title">Profile</h1>
-    </div>
-    <p>Name: {{ this.$store.state.user_store.username }}</p>
-    <p>Role: {{ this.$store.state.user_store.role }}</p>
-    <button
-      type="button"
-      :class="{btn:true, darkTheme:darkTheme}"
-      @click="darkTheme = !darkTheme"
-    >Dark theme: {{darkTheme}}</button>
-    <br />
-    <button type="button" class="btn btn-primary" @click="logout">Logout</button>
-    <br />
-    <br />
-    <h5>Change password:</h5>
+  <div class="profile">
+    <div class="container">
+      <div class="box">
+        <h1 class="title">Profile</h1>
+      </div>
+      <div class="box">
+        <p>Name: {{ this.$store.state.user_store.username }}</p>
+        <p>Role: {{ this.$store.state.user_store.role }}</p>
+        <button type="button" class="btn btn-primary" @click="logout">Logout</button>
+      </div>
+      <div class="box">
+        <p>Theme</p>
+        <button
+          type="button"
+          :class="{btn:true, darkTheme:darkTheme}"
+          @click="darkTheme = !darkTheme"
+        >Dark theme: {{darkTheme}}</button>
+      </div>
+      <div class="box">
+        <h5>Change password:</h5>
 
-    <form @submit.prevent="changePassword">
-      <span>Old password:</span>
-      <input type="password" v-model="oldpass" />
-      <span>New password:</span>
-      <input type="password" v-model="newpass" />
-      <button type="submit" class="btn btn-primary">Change</button>
-    </form>
-    <router-link
-      tag="button"
-      to="/admin"
-      class="btn btn-danger"
-      v-if="$store.state.user_store.role === 'admin'"
-    >Admin</router-link>
+        <form @submit.prevent="changePassword">
+          <span>Old password:</span>
+          <input type="password" v-model="oldpass" />
+          <span>New password:</span>
+          <input type="password" v-model="newpass" />
+          <button type="submit" class="btn btn-primary">Change</button>
+        </form>
+      </div>
+      <div class="box">
+        <p>Admin stuff</p>
+        <router-link
+          tag="button"
+          to="/admin"
+          class="btn btn-danger"
+          v-if="$store.state.user_store.role === 'admin'"
+        >Admin</router-link>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -84,9 +93,12 @@ export default {
 </script>
 <style scoped>
 .profile {
-  margin-top: 80px;
+  margin-top: 100px;
   margin-bottom: 80px;
   color: var(--color-text);
+  display: flex;
+  justify-content: center;
+  align-self: center;
 }
 .title {
   font-weight: bold;
@@ -114,5 +126,57 @@ form input {
 .darkTheme {
   background: black;
   color: var(--color-contrast);
+}
+.container {
+  max-width: 70%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-rows: minmax(200px, auto);
+  /* margin: 40px; */
+  grid-auto-flow: dense;
+  grid-gap: 10px;
+}
+.container .box {
+  background: var(--color-card);
+  padding: 20px;
+  display: grid;
+  place-items: center;
+  transition: 0.5s;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+.container .box:hover {
+  box-shadow: 6px 6px 6px black;
+  transform: translateY(-3px);
+  -webkit-transform: translateY(-3px);
+  -moz-transform: translateY(-3px);
+  -ms-transform: translateY(-3px);
+  -o-transform: translateY(-3px);
+}
+img {
+  object-fit: cover;
+}
+.container .box:nth-child(1) {
+  grid-column: span 2;
+  grid-row: span 1;
+}
+@media (max-width: 768px) {
+  .container {
+    width: 100%;
+    margin: 0 !important;
+  }
+  .container .box:hover {
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
+}
+@media (max-width: 991px) {
+  .container {
+    grid-template-columns: repeat(auto-fill, minmax(50%, 1fr));
+    grid-template-rows: minmax(auto, auto);
+  }
+  .container .box {
+    grid-row: unset !important;
+    grid-column: unset !important;
+  }
 }
 </style>
